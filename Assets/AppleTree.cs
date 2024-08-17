2d14381e4a0cb4140f77b4cb9c81b4508040fa89
demure512 <649818851@qq.com>
2024-08-17 14:12:14 +0800

    adding files

---
+++ Assets/AppleTree.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppleTree : MonoBehaviour
+{
+    [Header("Set in Inspector")]
+    public GameObject applePrefab;
+
+    public float speed = 1f;
+
+    public float leftAndRightEdge = 10f;
+
+    public float chanceForChangeDirection = 0.1f;
+
+    public float secondsBetweenAppleDrops = 1f;
+
+    //drop apples
+    void Start()
+    {
+
+    }
+
+    //dirction and movement
+    void Update()
+    {
+        //movement
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
+
+        if (pos.x < -leftAndRightEdge)
+        {
+            speed = Mathf.Abs(speed); //turn right
+        }
+        else if (pos.x > leftAndRightEdge)
+        {
+            speed = -Mathf.Abs(speed); //turn left
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (Random.value < chanceForChangeDirection)
+        {
+            speed *= -1; //change direction
+        }
+    }
+
+}
